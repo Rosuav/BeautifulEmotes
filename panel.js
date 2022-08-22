d36dfa10810595e037d72f7aa582b805dda047b2
Chris Angelico <rosuav@gmail.com>
2022-08-22 11:48:19 +1000

    Fetch the current channel's name for the display

--- panel.js
+++ panel.js
@@ -1,6 +1,23 @@
 import {choc, set_content, on, DOM} from "https://rosuav.github.io/choc/factory.js";
 const {B} = choc; //autoimport
-set_content("main p", [
-	"Hello, ",
-	B("world"),
-]);
+
+let auth = null;
+async function twitch(url, args) {
+	args = args || { };
+	args.headers = args.headers || { };
+	args.headers["client-id"] = auth.clientId;
+	args.headers.authorization = "Extension " + auth.helixToken;
+	return await (await fetch("https://api.twitch.tv/helix/" + url, args)).json();
+}
+Twitch.ext.onAuthorized(async a => {
+	//If you could get a JSON Web Security Token, how far from this planet would you throw it? Just curious.
+	auth = a;
+	console.log("Hello, " + a.userId);
+	console.log("I am " + a.channelId);
+	const self = await twitch("users?id=" + auth.channelId);
+	set_content("main p", [
+		"Welcome to Channel ",
+		B(self.data[0].display_name),
+	]);
+	
+});
